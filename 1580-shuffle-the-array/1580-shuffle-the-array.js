85a92ce5f053b775197461ef712ffffc8522feed
faslu rahman <153052386+fasluolassery@users.noreply.github.com>
2025-02-10 09:14:30 +0530

    Time: 70 ms (15.67%), Space: 52.4 MB (28.7%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let a = []
+    for (let i = 0; i < n; i++){
+        a.push(nums[i], nums[n + i])
+    }
+    return a
+};
